ca752c2c7d541c885dc86420c5325045e268116f
Mirza Usman <mirzausmanmu07842@gmail.com>
2024-06-15 09:24:12 +0500

    added theme changing conditions in theme component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { ThemeProvider } from "./contexts/theme";
+import Theme from "./components/Theme";
+import Card from "./components/Card";
 
 function App() {
   const [themeMode, setThemeMode] = useState("light")
@@ -10,17 +12,21 @@ function App() {
     setThemeMode("dark")
   }
   useEffect(()=>{
-    const htmlEl = document.querySelector("html").classList.remove("dark", "ligth")
-    htmlEl.classList.add(themeMode) 
+    document.querySelector("html").classList.remove("dark", "light")
+    document.querySelector("html").classList.add(themeMode) 
   },[themeMode])
   return (
 
     <ThemeProvider value={{lightTheme, darkTheme, themeMode}}>
     <div className="flex flex-wrap min-h-screen items-center">
       <div className="w-full">
-        <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
+        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+          <Theme/>
+        </div>
 
-        <div className="w-full max-w-sm mx-auto"></div>
+        <div className="w-full max-w-sm mx-auto">
+          <Card/>
+        </div>
       </div>
     </div>
     </ThemeProvider>
