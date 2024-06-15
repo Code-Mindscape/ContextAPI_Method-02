b0dea03b54d67f4a0e2aa4d9e7d6fca777848eaf
Mirza Usman <mirzausmanmu07842@gmail.com>
2024-06-15 08:50:16 +0500

    added functionality in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { ThemeProvider } from "./contexts/theme";
 
 function App() {
+  const [themeMode, setThemeMode] = useState("light")
+  const lightTheme =()=> {
+    setThemeMode("light")
+  }
+  const darkTheme =()=> {
+    setThemeMode("dark")
+  }
+  useEffect(()=>{
+    const htmlEl = document.querySelector("html").classList.remove("dark", "ligth")
+    htmlEl.classList.add(themeMode) 
+  },[themeMode])
   return (
+
+    <ThemeProvider value={{lightTheme, darkTheme, themeMode}}>
     <div className="flex flex-wrap min-h-screen items-center">
       <div className="w-full">
         <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
@@ -9,6 +23,7 @@ function App() {
         <div className="w-full max-w-sm mx-auto"></div>
       </div>
     </div>
+    </ThemeProvider>
   );
 }
 
